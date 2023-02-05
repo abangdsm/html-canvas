36609856e752f90e636ca09ae48397b5ff039b9e
abangdsm <id.reversal@gmail.com>
2023-02-06 00:59:13 +0700

    Menambahkan fungsi game.js

--- game.js
+++ game.js
@@ -1,10 +1,22 @@
 /** @type {HTMLCanvasElement} */
 
 function startGame(){
-    myGamePiece = new component(30, 30, 'red', 10, 120);
+    myGamePiece = new component(140, 30, 'red', 10, 120);
     myGamePiece.gravity = 0.05;
     myScore = new component('30px', 'Consolas', 'black', 280, 40, 'text');
     myGameArea.start();
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
 let myGameArea = {
@@ -14,9 +26,16 @@ let myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
         this.frameNo = 0;
     },
     clear: function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+
+function updateGameArea(){
+    // myGameArea.clear();
+    myGamePiece.x += 1;
+    myGamePiece.update();
 }
